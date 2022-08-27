b3cfda3f5fcee56fcdf86d60fdbaec7eef3e1bac
arich <arich@scwa.ca.gov>
2022-08-26 17:12:29 -0700

    updated runs

--- run_all.py
+++ run_all.py
@@ -8,29 +8,25 @@ import matplotlib.pyplot as plt
 import pond_inflows
 
 
-# for run in ['June2012', 'June2013', 'June2014', 'June2015', 'June2016', 'June2017', 'June2018', 'June2019', 'June2020', 'calibration']:
-# for run in ['June2018', 'June2019',  'June2020',]:
-for run in [ 'June2020',]:
 
-    # print(run)
-    # basic.setup_folder(run)
-    # write_inflows.run(model_name=run)
+
+for run in ['June2012', 'June2013', 'June2014', 'June2015',
+            'June2016', 'June2017', 'June2018', 'June2019',
+            'June2020']:
+
+    print(run)
+    basic.setup_folder(run)
+    write_inflows.run(model_name=run)
     # # SFRtoSWR.run(run)
-    # pond_inflows.run(run)
-    # m = basic.load_model()
-    #
-    # success, buffer = m.run_model(silent = False,)
-    #
-    # if success:
-    #     Hydrographs.run(run_name=run, reload = False)
-    #     postprocess.run(run)
-    #
-    #     post_process_heads.run(run_name=run, head_frequency=5)
-    #
-    # plt.close('all')
+    pond_inflows.run(run)
+    m = basic.load_model()
 
+    success, buffer = m.run_model(silent = False,)
 
-    Hydrographs.run(run_name=run, reload = False)
-    postprocess.run(run)
+    if success:
+        Hydrographs.run(run_name=run, reload = False)
+        postprocess.run(run, riv_only = True)
 
-    post_process_heads.run(run_name=run, head_frequency=5)
+        post_process_heads.run(run_name=run, head_frequency=5)
+
+    plt.close('all')
