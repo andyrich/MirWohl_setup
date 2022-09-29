28b0ca6e33934e761a97dd58e52056ef65c3f973
arich <arich@scwa.ca.gov>
2022-09-29 11:10:21 -0700

    updated runs

--- run_all.py
+++ run_all.py
@@ -8,24 +8,30 @@ import matplotlib.pyplot as plt
 import write_pond_inflows_rch
 import make_wells
 import initial_conditions
+import os
 
 
-
-
-
+# for run in [ 'June2019',
+#             'June2020']:
+#
 for run in ['June2012', 'June2013', 'June2014', 'June2015',
             'June2016', 'June2017', 'June2018', 'June2019',
             'June2020']:
+# for run in ['June2017']:
 
     print(run)
     basic.setup_folder(run)
+    # basic.plot_all_aquifer_props(m, run)
+    basic.copy_mod_files(run)
     make_wells.run(name=run)
     write_inflows.run(model_name=run)
     # # SFRtoSWR.run(run)
+    SFRtoSWR.plot_start_stage(None, os.path.join('versions', run))
     write_pond_inflows_rch.run(run)
 
     m = basic.load_model()
-    success = initial_conditions.rerun_for_initial_cond(m, 2)
+    basic.plot_all_aquifer_props(m, run)
+    success = initial_conditions.rerun_for_initial_cond(m, 3)
 
     success, buffer = m.run_model(silent = False,)
 
